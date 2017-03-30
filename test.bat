f4834f183a1542ed331e3eec669e22f82510281b
Jeff Ashton <Jeff.Ashton@D2L.com>
2017-03-30 00:36:07 -0400

    Drawing lines

--- test.bat
+++ test.bat
@@ -1,6 +1,3 @@
-nuget restore
-msbuild SetUpFixtureTests\bin\Debug\SetUpFixtureTests.dll
-
 .\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe .\SetUpFixtureTests\bin\Debug\SetUpFixtureTests.dll -where "cat  == foo" -result foo.result.xml
 .\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe .\SetUpFixtureTests\bin\Debug\SetUpFixtureTests.dll -where "cat == bar" -result bar.result.xml
 .\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe .\SetUpFixtureTests\bin\Debug\SetUpFixtureTests.dll -where "cat == foo || cat == bar" -result all.result.xml
