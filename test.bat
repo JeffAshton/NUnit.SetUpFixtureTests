cdc695ddd3ce836b87187001e25761d78fe08bcc
Jeff Ashton <Jeff.Ashton@D2L.com>
2017-03-30 00:36:55 -0400

    Not pausing

--- test.bat
+++ test.bat
@@ -1,5 +1,3 @@
 .\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe .\SetUpFixtureTests\bin\Debug\SetUpFixtureTests.dll -where "cat  == foo" -result foo.result.xml
 .\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe .\SetUpFixtureTests\bin\Debug\SetUpFixtureTests.dll -where "cat == bar" -result bar.result.xml
 .\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe .\SetUpFixtureTests\bin\Debug\SetUpFixtureTests.dll -where "cat == foo || cat == bar" -result all.result.xml
-
-@pause
